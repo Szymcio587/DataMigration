c8a48fcb7418e26a862cb84950112253ca2ee948
Szymcio587 <szymon.talar2882@gmail.com>
2023-07-02 15:59:17 +0200

    RouteBuilder updated

--- src/main/java/org/example/migration/MyRouteBuilder.java
+++ src/main/java/org/example/migration/MyRouteBuilder.java
@@ -2,18 +2,36 @@ package org.example.migration;
 
 import org.apache.camel.builder.RouteBuilder;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class MyRouteBuilder extends RouteBuilder {
+
+    private Properties properties;
+
+    public void ReadProperties() {
+        properties = new Properties();
+        try (FileInputStream fileInputStream = new FileInputStream("src/main/resources/database.properties")) {
+            properties.load(fileInputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     @Override
     public void configure() throws Exception {
-        from("file:src/main/resources/LegacySystemData")
-                .routeId("migrationRoute")
+        ReadProperties();
+        
+        from("file:src/main/resources/Scripts/V1")
+                .routeId("route1")
                 .onException(Exception.class)
                 .handled(true)
                 .to("log:error")
                 .end()
                 .split().tokenize("\n")
-                .to("jdbc:sourceDB")
-                .to("jdbc:targetDB")
+                .to(properties.getProperty("db.url").concat(properties.getProperty("db.source")))
+                .to(properties.getProperty("db.url").concat(properties.getProperty("db.target")))
                 .end();
     }
 }
